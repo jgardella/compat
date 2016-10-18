9626d804c9b69e7f96f49af639d3283970708cd0
Jason Gardella <jason.a.gardella@gmail.com>
2016-10-18 16:24:15 -0400

    Start implementing CLI.

--- src/compat.js
+++ src/compat.js
@@ -1,13 +1,46 @@
+let yargs = require('yargs')
+let fs = require('fs')
 let extract = require('./extract.js')
 let check = require('./check.js')
 let output = require('./output.js')
 
-let usedFeatures = extract.withAllFeatures(`
-  let x = 3;
-  let y = 4;
-  let z = { x, y }
-`)
+const argv =
+  yargs
+    .alias({
+      'target': 't',
+      'env': 'e'
+    })
+    .array(['target', 'env'])
+    .describe({
+      'target': 'file(s) and directories containing files to check for compatibility',
+      'env': 'environments to check for compatiblity with'
+    })
+    .default({
+      'target': ['.'],
+      'env': ['ie11', 'chrome54', 'firefox49', 'edge13', 'edge14', 'safari9', 'safari10']
+    })
+    .help()
+    .argv
 
-let errors = check.checkFeatureCompatibility(usedFeatures, ['ie11', 'chrome54'])
+const filesToCheck =
+  [].concat.apply([],
+    argv.target.map((fileName) => {
+      const isDir = fs.lstatSync(fileName).isDirectory()
+      if (isDir) {
+        return fs.readdirSync(fileName)
+          .filter((dirFileName) => { return dirFileName.endsWith('.js') })
+          .map((dirFileName) => { return fileName + '/' + dirFileName })
+      } else {
+        return [fileName]
+      }
+    })
+  )
 
-output.outputErrors(errors)
+filesToCheck.forEach((fileName) => {
+  let fileContents = fs.readFileSync(fileName, 'utf8')
+
+  let usedFeatures = extract.withAllFeatures(fileContents)
+  let errors = check.checkFeatureCompatibility(usedFeatures, argv.env)
+  console.log('In file "' + fileName + '":')
+  output.outputErrors(errors)
+})
