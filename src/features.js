0d0713b821c11b18834609c62980e758e7d8f052
Jason Gardella <jason.a.gardella@gmail.com>
2016-10-19 11:51:17 -0400

    Implement feature group to feature converter.

--- src/features.js
+++ src/features.js
@@ -2,19 +2,38 @@ let all = require('./features/all.js')
 let es6 = require('./features/es6/all.js')
 
 module.exports.getFeatures = (featureIds, ignoreFeatureIds) => {
-  let features = []
+  let featureGroups = []
 
   if (featureIds.indexOf('all') > -1) {
-    features = all
+    featureGroups.push(all.features)
   } else {
     if (featureIds.indexOf('es6') > -1) {
-      features = features.concat(es6)
+      featureGroups.push(es6.features)
     }
   }
+
+  let features = [].concat.apply([],
+    featureGroups.map((group) => { return flattenGroupToFeatures(group) })
+  )
 
   return features.filter((feature) => {
     return !ignoreFeatureIds.some((ignoreFeature) => {
       return feature.type === ignoreFeature
     })
   })
+}
+
+function flattenGroupToFeatures (group) {
+  let features = []
+
+  Object.keys(group).forEach((key) => {
+    const child = group[key]
+    if (child.type === undefined) {
+      features = features.concat(flattenGroupToFeatures(child))
+    } else {
+      features.push(child)
+    }
+  })
+
+  return features
 }
